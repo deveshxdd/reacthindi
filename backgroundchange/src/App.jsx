786b88464f6008de507d26cd24494b9b7926e21f
Devesh  Upadhyay <130208972+deveshxdd@users.noreply.github.com>
2023-10-17 21:30:36 +0530

    bc

--- backgroundchange/src/App.jsx
+++ backgroundchange/src/App.jsx
@@ -1,18 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Change from "./components/Change";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-      <Change color = "bg-red-400"/>
-      <Change color = "bg-green-400"/>
-      <Change color = "bg-blue-400"/>
-      <Change color = "bg-yellow-400"/>
+      <div className='bg-red'
+      style={{backgroundColor:'red'}}>
+        
+      </div>
     </>
   )
 }

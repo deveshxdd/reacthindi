850725e5c2dfaeae37d0e6b699db82d0b42b7286
Devesh  Upadhyay <130208972+deveshxdd@users.noreply.github.com>
2023-10-18 00:47:21 +0530

    project

--- randompassword/src/App.jsx
+++ randompassword/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState , useCallback } from 'react'
+import { useState , useCallback , useEffect , useRef} from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
@@ -8,8 +8,15 @@ function App() {
   const [number, setnumber] = useState(false)
   const [character, setcharacter] = useState(false)
   const [pass, setpass] = useState("")
+  const passref = useRef(null)
+ const copypasstoclipboard = useCallback(()=>{
+  passref.current?.select()
+  passref.current?.setSelectionRange(0,3)
+window.navigator.clipboard.writeText(pass)
+ },[pass])
+ 
 const Passwordgenerator = useCallback(()=>{
-let pass = ""
+let pas = ""
 let str = "ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghijklmnopqrstuvwxyz"
 if(number){
   str+="0123456789"
@@ -18,26 +25,30 @@ if(character){
   str+="!@$%^&*()_+=[]{}"
 }
 for(let i = 1; i <= length; i++){
-  let char = Math.floor(Math.random()*str.length()+1)
-   pass = str.charAt(char)
+  let char = Math.floor(Math.random()*str.length+1)
+   pas += str.charAt(char)
 }
-setpass(pass)
-},[length,number,character,pass])
+setpass(pas)
+},[length,number,character,setpass])
   
+useEffect(()=>{
+Passwordgenerator()
+},[length,number,character,Passwordgenerator])
 
   return (
     <>
     
       <div className="w-full max-w-md mx-auto shadow-md rounder-lg px-4 my-8 text-orange-500 bg-gray-700">
-      <h1 className='text- 4xl text-center text-white my-3'>Password generator</h1>
+      <h1 className='text- 4xl text-center text-white my-3'>Random Password Generator</h1>
     <div className="flex shadow rouder-lg overflow-hidden mb-4">
-      <input type="text" value={pass} placeholder='password' readOnly className= ' outline-none w-full py-1 px-3' />
-      <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink 0'>copy</button>
+      <input type="text" value={pass} placeholder='password' readOnly className= ' outline-none w-full py-1 px-3' ref={passref}/>
+      <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink 0'
+      onClick={copypasstoclipboard}>copy</button>
       
     </div>
     <div className='flex text-sm gap-x-2'>
         <div className='flex items-center gap-x-1'>
-          <input type="range" min={6} max={100}  value={length} className='cursor-pointer'
+          <input type="range" min={6} max={10}  value={length} className='cursor-pointer'
           onChange={(e)=>{
             setlength(e.target.value)
           }}
@@ -48,12 +59,12 @@ setpass(pass)
         <div className='flex items-center gap-x-1'>
           <input type="checkbox"  defaultChecked = {number} 
           onChange={ ()=>{
-            (prev)=>!prev
+            setnumber(prev=>!prev)
            } }/>
           <label>Number</label>
           <input type="checkbox"  defaultChecked = {character} 
           onChange={ ()=>{
-            (prev)=>!prev
+            setcharacter(prev=>!prev)
           }}/>
           <label>Character</label>
 
